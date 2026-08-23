858b3a2337c4b5fec13371e0a01a3bfca7a610f2
faulnam <syifakul.anm@gmail.com>
2026-08-23 18:10:54 +0700

    init

--- database/seeders/LuxuryProductSeeder.php
+++ database/seeders/LuxuryProductSeeder.php
@@ -32,6 +32,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Premium Silk Pashmina',
                 'description' => 'Pashmina eksklusif berbahan silk premium yang memberikan kilau mewah dan tekstur sangat lembut. Mudah dibentuk, tegak di dahi, dan tidak menerawang. Sempurna untuk acara formal maupun gaya elegan sehari-hari.',
                 'price' => 150000,
+                'discount_percent' => 0,
                 'stock' => 100,
                 'weight' => 200,
                 'image' => 'product-1-hijab.jpg',
@@ -43,6 +44,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Signature Instan Bergo',
                 'description' => 'Hijab instan bergo dengan pet antem (anti tembem) yang membingkai wajah dengan sempurna. Terbuat dari bahan jersey premium yang adem, flowy, dan anti-kusut. Pilihan tepat untuk tampil cantik tanpa ribet.',
                 'price' => 125000,
+                'discount_percent' => 50,
                 'stock' => 100,
                 'weight' => 250,
                 'image' => 'product-2-hijab.jpg',
@@ -54,6 +56,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Kala Ruffle Blouse',
                 'description' => 'Blouse cantik dengan detail ruffle di bagian lengan dan dada yang memberikan kesan feminin nan elegan. Terbuat dari katun linen yang breathable, cocok dipadukan dengan celana kulot atau rok untuk ke kantor maupun hangout.',
                 'price' => 250000,
+                'discount_percent' => 0,
                 'stock' => 100,
                 'weight' => 300,
                 'image' => 'product-1-baju.jpg',
@@ -65,6 +68,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Nayla Midi Dress',
                 'description' => 'Midi dress berpotongan A-line yang anggun dengan aksen tali pinggang yang bisa dilepas pasang. Material maxmara lux yang jatuh dan tidak mudah kusut. Tampil effortless elegan untuk acara spesial Anda.',
                 'price' => 350000,
+                'discount_percent' => 50,
                 'stock' => 100,
                 'weight' => 350,
                 'image' => 'product-2-baju.jpg',
@@ -76,6 +80,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Luna Highwaist Kulot',
                 'description' => 'Celana kulot berpotongan high-waist yang membuat ilusi kaki lebih jenjang dan pinggang lebih ramping. Berbahan scuba premium yang stretch dan rapi, nyaman dipakai seharian untuk gaya kasual hingga profesional.',
                 'price' => 195000,
+                'discount_percent' => 0,
                 'stock' => 100,
                 'weight' => 400,
                 'image' => 'product-1-celana.jpg',
@@ -87,6 +92,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Amira Pleated Skirt',
                 'description' => 'Rok plisket premium dengan lipatan konsisten yang tidak mudah hilang meski dicuci berulang kali. Karet pinggang yang elastis dan material hyget super yang flowy menjadikannya andalan untuk gaya OOTD hijab Anda.',
                 'price' => 180000,
+                'discount_percent' => 50,
                 'stock' => 100,
                 'weight' => 350,
                 'image' => 'product-2-celana.jpg',
@@ -98,6 +104,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Chloe Classic Mules',
                 'description' => 'Sepatu mules bergaya klasik dengan detail pointed toe yang memberikan kesan kaki lebih ramping. Sol empuk dan material vegan leather berkualitas tinggi menjamin kenyamanan maksimal saat melangkah.',
                 'price' => 280000,
+                'discount_percent' => 0,
                 'stock' => 100,
                 'weight' => 500,
                 'image' => 'product-1-sandal.jpg',
@@ -109,6 +116,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Bella Block Heels',
                 'description' => 'Heels berhak tahu (block heels) 5 cm yang super nyaman dan stabil digunakan berjalan seharian. Aksen strap yang minimalis membuatnya mudah dipadupadankan dengan dress maupun celana untuk tampilan chic.',
                 'price' => 320000,
+                'discount_percent' => 50,
                 'stock' => 100,
                 'weight' => 550,
                 'image' => 'product-2-sandal.jpg',
@@ -120,6 +128,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Anya Quilted Tote',
                 'description' => 'Tote bag bertekstur quilted yang mewah dan elegan. Kompartemen sangat luas, muat laptop hingga 14 inch, pouch makeup, dan mukena. Material kulit sintetis yang tebal memastikan tas ini awet dan kokoh.',
                 'price' => 350000,
+                'discount_percent' => 0,
                 'stock' => 50,
                 'weight' => 800,
                 'image' => 'product-1-tas.jpg',
@@ -131,6 +140,7 @@ class LuxuryProductSeeder extends Seeder
                 'name' => 'Zara Chain Sling Bag',
                 'description' => 'Tas selempang berdesain timeless dengan kombinasi rantai emas elegan. Ukuran yang pas untuk esensial harian seperti ponsel, lipstik, dan dompet. Sempurna untuk melengkapi gaya hangout atau menghadiri pesta santai.',
                 'price' => 290000,
+                'discount_percent' => 50,
                 'stock' => 50,
                 'weight' => 400,
                 'image' => 'product-2-tas.jpg',
@@ -150,11 +160,15 @@ class LuxuryProductSeeder extends Seeder
         ];
 
         foreach ($productsData as $data) {
+            $discountPercent = $data['discount_percent'] ?? 0;
             $product = Product::create([
                 'name' => $data['name'],
                 'slug' => Str::slug($data['name']),
                 'description' => $data['description'],
                 'price' => $data['price'],
+                'discount_percent' => $discountPercent,
+                'discount_start' => $discountPercent > 0 ? now() : null,
+                'discount_end' => $discountPercent > 0 ? now()->addYears(2) : null,
                 'stock' => $data['stock'],
                 'weight' => $data['weight'],
                 'image' => $data['image'],
@@ -178,18 +192,14 @@ class LuxuryProductSeeder extends Seeder
                     
                     ProductVariant::create([
                         'product_id' => $product->id,
-                        'name' => $size . ' - ' . $colorName,
-                        // Storing hex in image field temporarily since we don't have a color_hex field in variants.
-                        // Or we could encode it in the name. Let's encode it in a structured way if needed, 
-                        // but since the UI needs hex, maybe we can append it to the name.
-                        // Actually, looking at the UI, we need to extract colors. 
-                        // I will append [hex] to the variant name so it can be parsed in the view.
+                        'name' => $size . '|' . $colorName . '|' . $colorInfo['hex'],
                         'image' => null, 
                         'stock' => 10,
                         'price' => $product->price,
+                        'discount_percent' => $discountPercent,
+                        'discount_start' => $product->discount_start,
+                        'discount_end' => $product->discount_end,
                         'is_active' => true,
-                        // We use a convention for the name: Size | ColorName | #hexcode
-                        'name' => $size . '|' . $colorName . '|' . $colorInfo['hex']
                     ]);
                 }
             }
